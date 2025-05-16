d1703ae7c5a5b8e13b141ad81465570c52aeb4f9
yuhangle <893033655@qq.com>
2025-05-16 19:55:14 +0800

    first commit

---
+++ src/ecleaner.cpp
@@ -0,0 +1,22 @@
+//
+// Created by yuhang on 2025/4/27.
+//
+
+#include "ecleaner.h"
+
+ENDSTONE_PLUGIN("ecleaner", "0.1.1", ECleaner)
+{
+    description = "My first C++ plugin for Endstone servers";
+
+    command("ecl")
+            .description("ECleaner")
+            .usages("/ecl",
+                    "/ecl clean [entity|item]",
+                    "/ecl reload"
+                    )
+            .permissions("ecleaner.command.op");
+
+    permission("ecleaner.command.op")
+            .description("ecl op command")
+            .default_(endstone::PermissionDefault::Operator);
+}
